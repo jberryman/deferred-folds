62b7bf271e2737ba23e7426916a9673f8d2e35cf
Brandon Simmons <brandon.m.simmons@gmail.com>
2021-08-23 15:28:25 -0400

    Compatibility with https://github.com/haskell/text/pull/365

--- library/DeferredFolds/Util/TextArray.hs
+++ library/DeferredFolds/Util/TextArray.hs
@@ -6,7 +6,7 @@ import Data.Text.Array
 import qualified Data.Text.Internal as TextInternal
 import qualified Data.Text.Internal.Encoding.Utf16 as TextUtf16
 import qualified Data.Text.Internal.Unsafe.Char as TextChar
-
+import qualified Data.Text.Unsafe as T
 
 {-|
 Same as 'Data.Text.Unsafe.iter',
@@ -16,15 +16,5 @@ uses a continuation and passes the next offset to it instead of delta.
 {-# INLINE iter #-}
 iter :: Array -> Int -> (Char -> Int -> a) -> a
 iter arr offset cont =
-  let
-    b1 =
-      unsafeIndex arr offset
-    in if b1 >= 0xd800 && b1 <= 0xdbff
-      then let
-        b2 =
-          unsafeIndex arr (succ offset)
-        char =
-          TextUtf16.chr2 b1 b2
-        in cont char (offset + 2)
-      else
-        cont (TextChar.unsafeChr b1) (offset + 1)
+    case T.iterArray arr offset of
+      T.Iter chr l -> cont chr (offset+l)
